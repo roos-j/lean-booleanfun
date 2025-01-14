082c9ddd64d8e11d2d707750d1483ff15cd50805
Joris Roos <roos.jor@gmail.com>
2025-01-14 07:53:00 -0500

    update mathlib and Lean

--- BooleanFun/AuxLemmas.lean
+++ BooleanFun/AuxLemmas.lean
@@ -3,7 +3,7 @@ Copyright (c) 2024 Joris Roos. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joris Roos
 -/
-import Mathlib.Algebra.BigOperators.Group.Finset
+import Mathlib.Algebra.BigOperators.Group.Finset.Basic
 import Mathlib.Algebra.BigOperators.Finprod
 import Mathlib.Data.Real.Irrational
 
